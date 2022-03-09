387a4c5922b0646779235abdb97b150d1f8c219b
Bjarne <83133360+BjarneVerschorre@users.noreply.github.com>
2022-03-09 20:56:44 +0100

    Added protection against SQL Injection

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const port = process.env.PORT || 1000;
 const mysql = require('mysql')
 const bcrypt = require('bcrypt');
+const sanitizeHtml = require('sanitize-html');
+
+const port = process.env.PORT || 1000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -13,48 +18,68 @@ db.connect((err) => {
     if(err){ throw err };
     console.log("MySQL Connected");
 });
-const app = express();
-app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/login.html");
+    console.log(__dirname)
+    res.sendFile(__dirname + "/static/home.html");
+})
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/static/login.html");
+})
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/static/register.html");
 })
 
-app.post("/login", async (req, res) => {
-    const { user, password } = req.body;
+app.post("/api/login", async (req, res) => {
+    const { username, password } = req.body;
+    if(req.body.length < 2 ) {return res.status(400).json({err:"Empty fields"})}
+    username = sanitizeHtml(username,{allowedTags: [], allowedAttributes: {}})
+    password = sanitizeHtml(password,{allowedTags: [], allowedAttributes: {}})
 
-    const sql = `SELECT username FROM logindata WHERE username = "${user}"`
-    db.query(sql, async (err, result) => {
+    verify(secret, token)
+    .then((data) => {
+        if (data.success === true) {
+        console.log('success!', data);
+        } else {
+        console.log('verification failed');
+        }
+    })
+    .catch(console.error);
+
+    db.query("SELECT username FROM logindata WHERE username = ?",[username], async (err, result) => {
         if(err) throw err;
-        if (result[0].username == user){
+        if (result.length > 0){
 
-            db.query(`SELECT password FROM logindata WHERE username="${user}"`, async (err, result) => {
+            db.query("SELECT password FROM logindata WHERE username=?",[username], async (err, result) => {
                 if(await bcrypt.compare(password, result[0].password)){
                     console.log("LOGGED IN");
+                    return res.redirect("/login")
                 } else {
                     console.log("Invalid Password")
                 }
             });
-
-
         }
     });
+    return res.status(400).json({err:"Invalid username or password"})
 })
 
-app.post("/register", async (req, res) => {
-    const { user, password } = req.body;
+app.post("/api/register", async (req, res) => {
+    const { username, password } = req.body;
+    if(req.body.length < 2 ) {return res.status(400).json({err:"Empty fields"})}
+    username = sanitizeHtml(username,{allowedTags: [], allowedAttributes: {}})
+    password = sanitizeHtml(password,{allowedTags: [], allowedAttributes: {}})
 
-    const sql = `SELECT username FROM logindata WHERE username = "${user}"`
-    db.query(sql, async (err, result) =>  {
+    db.query("SELECT username FROM logindata WHERE username = ?",[username], async (err, result) =>  {
         if(err) throw err;
         if (result.length > 0) {
             console.log("Username already in use")
-            return 
+            return res.status(400).json({err:"User already exists"})
         }
         const hashedPass = await bcrypt.hash(password, 10)
-        db.query(`INSERT INTO logindata SET ?`,{username:user, password:hashedPass}, (err, result) => {
+        db.query("INSERT INTO logindata SET ?",[{username:username, password:hashedPass}], (err, result) => {
             if(err) throw err;
             console.log("REGISTERED")
+            res.redirect("/login")
         });
     });
 })
